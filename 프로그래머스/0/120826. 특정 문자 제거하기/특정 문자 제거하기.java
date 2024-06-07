bead4e319eb11286c781ca6831e74fd46b7ad8f4
Hye Jin <127267658+hjin111@users.noreply.github.com>
2024-06-07 11:05:08 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 1.17 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,12 +1,11 @@
 class Solution {
     public String solution(String my_string, String letter) {
-       String answer = "";
+        String answer = "";
         for(int i = 0; i < my_string.length(); i++){
-            if(my_string.charAt(i) != letter.charAt(0)){
-                answer += my_string.charAt(i);
-            }
+           if(!(my_string.substring(i,i+1).equals(letter))){
+               answer += my_string.substring(i,i+1);
+           }
         }
-        
         return answer;
 
     }
