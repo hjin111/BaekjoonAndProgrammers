413a48843b612d71b494de928d590d1bda26835b
Hye Jin <127267658+hjin111@users.noreply.github.com>
2024-06-23 20:17:59 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 212 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i < t; i++){
+            int n = sc.nextInt();
+            
+            if (n == 1) {
+                System.out.println(1);
+                continue;
+            } else if (n == 2) {
+                System.out.println(2);
+                continue;
+            } else if (n == 3) {
+                System.out.println(4);
+                continue;
+            }
+            
+            int[] nArr = new int[n];
+            nArr[0] = 1;
+            nArr[1] = 2;
+            nArr[2] = 4;
+            
+            for(int j = 3; j < n; j++){
+                nArr[j] = nArr[j - 1] + nArr[j - 2] + nArr[j - 3];
+            }
+            System.out.println(nArr[n - 1]);
+        }
+    }
+}
