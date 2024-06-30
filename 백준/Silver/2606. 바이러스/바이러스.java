7610063f87d8f901a656d778a0a0a5612476604c
Hye Jin <127267658+hjin111@users.noreply.github.com>
2024-06-30 19:37:31 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    static List<List<Integer>> list;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int computer = Integer.parseInt(br.readLine());
+        int network = Integer.parseInt(br.readLine());
+
+        list = new ArrayList<>();
+        visited = new boolean[computer + 1];
+
+        for(int i = 0; i <= computer; i++){
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < network; i++) {
+            String[] input = br.readLine().split(" ");
+            int one = Integer.parseInt(input[0]);
+            int two = Integer.parseInt(input[1]);
+            list.get(one).add(two);
+            list.get(two).add(one);
+        }
+
+        // System.out.println(Arrays.deepToString(arr));
+
+//        for (int i = 0; i < network; i++) {
+//            String[] input = br.readLine().split(" ");
+//            int index = Integer.parseInt(input[0]);
+//            int value = Integer.parseInt(input[1]);
+//            for (int j = 0; j < arr[0].length; j++) {
+//                if(arr[index][j] == 0){
+//                    arr[index][j] = value;
+//                    break;
+//                }
+//            }
+//            list.add(new ArrayList<>());
+//        }
+
+        int answer = dfs(1);
+        System.out.println(answer - 1);
+
+    }
+    static int dfs(int start) {
+        visited[start] = true;
+        int count = 1;
+        for(int i : list.get(start)){
+            if(!visited[i]){
+                count += dfs(i);
+            }
+        }
+        return count;
+    }
+}
